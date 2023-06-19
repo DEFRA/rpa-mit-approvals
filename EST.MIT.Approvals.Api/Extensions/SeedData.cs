502bab755091636bf96d9271e1c7884d6bc906d8
Faisal Shehzad <faisal.shehzad@defra.gov.uk>
2023-06-19 15:42:33 +0100

    188894, Added in simple fake scheme to reference data

---
+++ EST.MIT.Approvals.Api/Extensions/SeedData.cs
@@ -0,0 +1,29 @@
+﻿using EST.MIT.Approvals.Data;
+using System.Diagnostics.CodeAnalysis;
+using EST.MIT.Approvals.Data.SeedData;
+
+namespace EST.MIT.Approvals.Api.Extensions;
+
+/// <summary>
+/// Seed data extension methods
+/// </summary>
+public static class SeedData
+{
+    /// <summary>
+    /// Extension method that calls the SeedProvider to seed reference data
+    /// into the application <see cref="ApprovalsContext" />.
+    /// </summary>
+    /// <param name="app">Application builder</param>
+    [ExcludeFromCodeCoverage]
+    public static void UseReferenceDataSeeding(this IApplicationBuilder app)
+    {
+        var scopeFactory = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>();
+
+        using var serviceScope = scopeFactory.CreateScope();
+
+        var context = serviceScope.ServiceProvider.GetRequiredService<ApprovalsContext>();
+        var logger = serviceScope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+        SeedProvider.SeedReferenceData(context, logger);
+    }
+}
--- EST.MIT.Approvals.Api/Program.cs
+++ EST.MIT.Approvals.Api/Program.cs
@@ -1,5 +1,6 @@
 using Approvals.Api.Endpoints;
 using EST.MIT.Approvals.Api.endpoints;
+using EST.MIT.Approvals.Api.Extensions;
 using EST.MIT.Approvals.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -22,6 +23,11 @@ builder.Services.AddSwaggerServices();
 builder.Services.AddApprovalsServices();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment() && args.Contains("--seed-ref-data"))
+{
+    app.UseReferenceDataSeeding();
+}
 
 app.SwaggerEndpoints();
 app.MapHealthCheckGetEndpoints();
