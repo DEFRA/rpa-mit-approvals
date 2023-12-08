a79091906a21776492ed234515872b24c2c61eba
Gibbons <andy.gibbons@defra.gov.uk>
2023-12-08 12:00:23 +0000

    Log CreateScripts for Seeding to File (WIP)

--- EST.MIT.Approvals.Data/ApprovalsContext.cs
+++ EST.MIT.Approvals.Data/ApprovalsContext.cs
@@ -1,5 +1,6 @@
 ﻿using EST.MIT.Approvals.Data.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System.Diagnostics.CodeAnalysis;
 
 namespace EST.MIT.Approvals.Data;
@@ -8,13 +9,21 @@ namespace EST.MIT.Approvals.Data;
 public class ApprovalsContext : DbContext
 {
     public DbSet<ApproverEntity> Approvers => Set<ApproverEntity>();
-
     public DbSet<SchemeEntity> Schemes => Set<SchemeEntity>();
     public DbSet<ApprovalGroupEntity> ApprovalGroups => Set<ApprovalGroupEntity>();
     public DbSet<ApproverApprovalGroupEntity> ApproverAprovalGroups => Set<ApproverApprovalGroupEntity>();
 
     public ApprovalsContext(DbContextOptions<ApprovalsContext> options) : base(options)
     {
+    }
+
+    private readonly StreamWriter _logStream = new StreamWriter("MIT_Approvals_Seed_SQL.log", append: true);
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        //optionsBuilder.LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Name });
+        optionsBuilder.LogTo(_logStream.WriteLine, new[] { DbLoggerCategory.Database.Name });
+        optionsBuilder.LogTo(_logStream.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information);
+        optionsBuilder.EnableSensitiveDataLogging();
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
