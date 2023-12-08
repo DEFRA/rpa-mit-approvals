72748cc4bde30eeb61efdf3ae686c87e836cb4d4
Gibbons <andy.gibbons@defra.gov.uk>
2023-12-08 11:41:01 +0000

    Adjust DataContact for the Approver-ApprovalGroups Models

--- EST.MIT.Approvals.Data/ApprovalsContext.cs
+++ EST.MIT.Approvals.Data/ApprovalsContext.cs
@@ -20,10 +20,10 @@ public class ApprovalsContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<ApproverEntity>()
-            .HasMany(e => e.Schemes)
+            .HasMany(e => e.ApprovalGroups)
             .WithMany(e => e.Approvers)
-            .UsingEntity<ApproverSchemeEntity>(
-                l => l.HasOne<SchemeEntity>().WithMany().HasForeignKey(e => e.SchemeId),
+            .UsingEntity<ApproverApprovalGroupEntity>(
+                l => l.HasOne<ApprovalGroupEntity>().WithMany().HasForeignKey(e => e.ApprovalGroupId),
                 r => r.HasOne<ApproverEntity>().WithMany().HasForeignKey(e => e.ApproverId));
     }
 }
