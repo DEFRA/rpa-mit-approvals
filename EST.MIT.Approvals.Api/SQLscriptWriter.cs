4eccf63a48ede2962e5b2915d5531acd895855e1
Gibbons, Andy <Andy.Gibbons@defra.gov.uk>
2024-01-03 16:36:26 +0000

    Export SQL script for locally seeded DB

---
+++ EST.MIT.Approvals.Api/SQLscriptWriter.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace EST.MIT.Approvals.Api
+{
+    public class SQLscriptWriter: TextWriter
+    {
+        private StreamWriter? _streamWriter;
+        private readonly string _fileName;
+
+        public override Encoding Encoding => throw new NotImplementedException();
+
+        public SQLscriptWriter(string fileName)
+        {
+            _fileName = fileName;
+        }
+
+        public void Open()
+        {
+            _streamWriter = new StreamWriter(_fileName, append:true);
+            _streamWriter.AutoFlush = true;
+        }
+
+        public override void Close()
+        {
+            _streamWriter?.Close();
+            _streamWriter?.Dispose();
+        }
+
+        public override void WriteLine(string? value)
+        {
+            _streamWriter?.WriteLine(value);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _streamWriter?.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
